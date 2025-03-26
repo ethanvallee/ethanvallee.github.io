2ddb02ff8c53b390f3736328e47283884829430a
ethanvallee <ethanlanevallee@gmail.com>
2025-03-26 18:43:42 +0000

    Starting Step 6

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -29,7 +29,8 @@ var background = function (window) {
         // ANIMATION VARIABLES HERE //////////////////////////////////////
         //////////////////////////////////////////////////////////////////
         // TODO (several):
-      
+        var tree; 
+        var buildings = [];
       
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
@@ -60,10 +61,21 @@ var background = function (window) {
 
 
             // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
+            for (var i = 0; i < 5; ++i) {
+                var buildingColors = ["red", "blue", "black", "purple", "orange"]
+                var buildingHeight = 300 * Math.random();
+                var building = draw.rect(75, buildingHeight, buildingColors[i], "Black", 1);
+                building.x = 200 * i;
+                building.y = groundY - buildingHeight;
+                background.addChild(building);
+                buildings.push(building);
+              }
             
             // TODO 3: Part 1 - Add a tree
-            
+            tree = draw.bitmap("img/tree.png");
+            tree.x = canvasWidth - 300;
+            tree.y = groundY -225;
+            background.addChild(tree);
             
         } // end of render function - DO NOT DELETE
         
@@ -77,10 +89,21 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 3: Part 2 - Move the tree!
-            
+            tree.x = tree.x - 1;
+
+            if (tree.x < -200) {
+             tree.x = canvasWidth;
+            }
             
             // TODO 4: Part 2 - Parallax
+            for (var i=0; i < buildings.length; i++ ){
+                var building = buildings[i];
+                building.x -= Math.random();
+               if (building.x < -100){
+                building.x = canvasWidth;
+               }
             
+            }
 
         } // end of update function - DO NOT DELETE
         
