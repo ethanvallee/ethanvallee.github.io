ec820693c62149cd76150e4dbe09752bf72ddee7
ethanvallee <ethanlanevallee@gmail.com>
2025-03-27 19:30:09 +0000

    Up to Step 8B

--- projects/runtime/js/runLevels.js
+++ projects/runtime/js/runLevels.js
@@ -18,14 +18,38 @@ var runLevels = function (window) {
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
-    var hitZoneSize = 25;
-    var damageFromObstacle = 10;
-    var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
-    sawBladeHitZone.x = 400;
-    sawBladeHitZone.y = 380;
-    game.addGameItem(sawBladeHitZone);
-    var obstacleImage = draw.bitmap("img/sawblade.png");
-    sawBladeHitZone.addChild(obstacleImage);
+    
+    function createObstacles(x,y, hitSize, damage) {
+    var hitZoneSize = 25; //Defines size of hitzone
+    var damageFromObstacle = 10; //How much damage Hall-E takes from the saw
+    var obstacleHitZone = game.createObstacle(hitZoneSize, damageFromObstacle); //Creates obstacle with size parameters
+    obstacleHitZone.x = x; // X coordinate
+    obstacleHitZone.y = y; //Y Coordinate
+    game.addGameItem(obstacleHitZone); //Adds obstacle hitzone
+    var obstacleImage = draw.bitmap("img/sawblade.png"); //Makes the image of the saw
+    obstacleHitZone.addChild(obstacleImage); //Attaches image
+    obstacleImage.x = -25; //Positions images on hitzone by moving left
+    obstacleImage.y = -25; //Positions images on hitzone by moving down
+    obstacleHitZone.rotationalVelocity = 10;
+    } 
+
+    createObstacles(400, groundY - 50, 25, 10);
+    createObstacles(500, groundY - 50, 100, 25);
+    
+    var enemy = game.createGameItem("enemy", 25); //Creates enemy and adds to game
+    var redSquare = draw.rect(50, 50, "red"); //Creates a red square in the red square variable
+    redSquare.x = -25; // offsets the image from the hitzone by -25 pixels
+    redSquare.y = -25; // offsets the image from the hitzone by -25 pixels
+    enemy.addChild(redSquare); //adds red square as child
+    enemy.x = 400; //X of enemy
+    enemy.y = groundY - 50; //Y of enemy
+    game.addGameItem(enemy); //Adds item to game
+    enemy.velocityX -= 3; //How fast the box spins
+    enemy.rotationalVelocity = 10; //Sets rotational velocity of enemy
+    enemy.onPlayerCollision = function () {
+      game.changeIntegrity(-10) //Subtracts 10 health from Halle 
+    };
+    enemy.onProjecti  leCollision
 
 
     function startLevel() {
