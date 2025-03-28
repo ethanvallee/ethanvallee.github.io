c0f046a65b3077d5ef1fdc062032de96d70c6555
ethanvallee <ethanlanevallee@gmail.com>
2025-03-28 19:36:24 +0000

    Day 5, Step11

--- projects/runtime/js/runLevels.js
+++ projects/runtime/js/runLevels.js
@@ -36,20 +36,69 @@ var runLevels = function (window) {
     createObstacles(400, groundY - 50, 25, 10);
     createObstacles(500, groundY - 50, 100, 25);
     
-    var enemy = game.createGameItem("enemy", 25); //Creates enemy and adds to game
-    var redSquare = draw.rect(50, 50, "red"); //Creates a red square in the red square variable
-    redSquare.x = -25; // offsets the image from the hitzone by -25 pixels
-    redSquare.y = -25; // offsets the image from the hitzone by -25 pixels
-    enemy.addChild(redSquare); //adds red square as child
-    enemy.x = 400; //X of enemy
-    enemy.y = groundY - 50; //Y of enemy
-    game.addGameItem(enemy); //Adds item to game
-    enemy.velocityX -= 3; //How fast the box spins
-    enemy.rotationalVelocity = 10; //Sets rotational velocity of enemy
-    enemy.onPlayerCollision = function () {
-      game.changeIntegrity(-10) //Subtracts 10 health from Halle 
-    };
-    enemy.onProjecti  leCollision
+    function createEnemy(x, y, speed, health) {
+      var enemy = game.createGameItem("enemy", 25); //Creates enemy and adds to game
+      var redSquare = draw.rect(50, 50, "red"); //Creates a red square in the red square variable
+      redSquare.x = -25; // offsets the image from the hitzone by -25 pixels
+      redSquare.y = -25; // offsets the image from the hitzone by -25 pixels
+      enemy.addChild(redSquare); //adds red square as child
+      enemy.x = x; //X of enemy
+      enemy.y = y; //Y of enemy
+      game.addGameItem(enemy); //Adds item to game
+      enemy.velocityX -= speed; //How fast the box spins
+      enemy.rotationalVelocity = 10; //Sets rotational velocity of enemy
+      enemy.onPlayerCollision = function () {
+        game.changeIntegrity(health) //Subtracts 10 health from Halle 
+      };
+      
+      enemy.onProjectileCollision = function () {
+        game.increaseScore(100);  //Player gains 100 pts if enemy is defeated
+        enemy.flyTo(0,0); //Flies off screen
+      };
+    }
+    
+    createEnemy(400, groundY - 50, 3, -10);
+    createEnemy(800, groundY - 50, 10, -20);
+    createEnemy(1200, groundY - 50, 50, -15);
+   
+    function createReward(x, y, speed, health) {
+      var reward = game.createGameItem("reward", 25); //Creates reward and adds to game
+      var blueSquare = draw.rect(50, 50, "blue"); //Creates a blue square in the blue square variable
+      blueSquare.x = -25; // offsets the image from the hitzone by -25 pixels
+      blueSquare.y = -25; // offsets the image from the hitzone by -25 pixels
+      reward.addChild(blueSquare); //adds blue square as child
+      reward.x = x; //X of reward
+      reward.y = y; //Y of reward
+      game.addGameItem(reward); //Adds item to game
+      reward.velocityX -= speed; //How fast the box spins
+      reward.rotationalVelocity = 10; //Sets rotational velocity of reward
+      reward.onPlayerCollision = function () {
+        game.increaseScore(50);
+        game.changeIntegrity(health) //Subtracts 10 health from Halle 
+      reward.shrink();
+      };
+    }  
+      createReward(1000, groundY - 100, 3, 50);
+
+
+      function createLevel(x, y, speed) {
+        var reward = game.createGameItem("reward", 25); //Creates reward and adds to game
+        var yellowSquare = draw.rect(50, 50, "yellow"); //Creates a yellow square in the yellow square variable
+        yellowSquare.x = -25; // offsets the image from the hitzone by -25 pixels
+        yellowSquare.y = -25; // offsets the image from the hitzone by -25 pixels
+        reward.addChild(yellowSquare); //adds yellow square as child
+        reward.x = x; //X of reward
+        reward.y = y; //Y of reward
+        game.addGameItem(reward); //Adds item to game
+        reward.velocityX -= speed; //How fast the box spins
+        reward.rotationalVelocity = 10; //Sets rotational velocity of reward
+        reward.onPlayerCollision = function () {
+        reward.shrink();
+        startLevel();
+        };
+      }  
+        createLevel(1500, groundY-50,3 );
+  
 
 
     function startLevel() {
